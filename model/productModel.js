dabf0bbdccbcd1c31c77ff91e9fe775b399f1200
Fatma Alghonaimy <ffatmaalaa32@gmail.com>
2024-05-02 02:00:30 +0300

    intial Product Model

---
+++ model/productModel.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+//REVIEW; ??
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter Your Product Name"],
+  },
+  productID: {
+    type: String,
+    default: null,
+    unique: true,
+  },
+  category: {
+    type: String,
+    unique: true,
+    required: [true, "Please Enter Your Product Category"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please Enter Your Product description"],
+  },
+  stock: {
+    type: Number,
+    default: 0,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+    min: 0,
+    max: 5,
+  },
+  originalPrice: {
+    type: Number,
+    required: [true, "Please Enter Your Product Price"],
+  },
+  currentPrice: {
+    type: Number,
+    default: function() {
+      return this.originalPrice;
+    },
+  },
+  size: {
+    type: String,
+    default: "One Size",
+  },
+  color: [{ type: String, default: null }],
+  productPhotos: [{ type: String, default: null }],
+  createdAt: { type: Date, default: Date.now() },
+  active: {
+    type: Boolean,
+    select: false,
+    default: true,
+  },
+});
+
+productSchema.pre(/^find/, function(next) {
+  this.find({ active: { $ne: false } });
+  next();
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
