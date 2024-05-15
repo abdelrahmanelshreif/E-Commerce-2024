24df718f737150b52d165b0aadf1249ffd89288c
Abdelrahman Elshreif <link93955@gmail.com>
2024-05-15 18:01:51 +0300

    new edition on models and controllers

--- model/productModel.js
+++ model/productModel.js
@@ -1,51 +1,34 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  sold: {
-    type: Number,
-    default: 0
-  },
-  images: {
-    type: [String],
-    default: []
-  },
-  subcategory: {
-    type: [{
-      _id: mongoose.Schema.Types.ObjectId,
-      name: String,
-      slug: String,
-      category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category'
-      }
-    }],
-    default: []
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
   title: {
     type: String,
-    required: [true, "Please Enter Your Product Name"]
+    required: true
   },
   slug: {
     type: String,
-    required: true,
-    unique: true
+    required: true
   },
   description: {
     type: String,
-    required: [true, "Please Enter Your Product description"]
+    required: true
   },
   quantity: {
     type: Number,
-    default: 0
+    required: true
   },
   price: {
     type: Number,
-    required: [true, "Please Enter Your Product Price"]
+    required: true
   },
+  sold: {
+    type: Number,
+    required: true
+  },
+  images: [{
+    type: String,
+    required: true
+  }],
   imageCover: {
     type: String,
     required: true
@@ -53,63 +36,83 @@ const productSchema = new mongoose.Schema({
   category: {
     _id: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Category'
+      ref: 'Category',
+      required: true
     },
-    name: String,
-    slug: String,
-    image: String
+    name: {
+      type: String,
+      required: true
+    },
+    slug: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
   },
+  subcategory: [{
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Subcategory',
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    slug: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true
+    }
+  }],
   brand: {
     _id: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Brand'
+      ref: 'Brand',
+      required: true
     },
-    name: String,
-    slug: String,
-    image: String
+    name: {
+      type: String,
+      required: true
+    },
+    slug: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
   },
   ratingsAverage: {
     type: Number,
-    default: 0
+    required: true
+  },
+  ratingsQuantity: {
+    type: Number,
+    required: true
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    // required: true
   },
   updatedAt: {
     type: Date,
-    default: Date.now
-  }
+    // required: true
+  },
+  reviews: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Review'
+  }]
 });
 
-
-productSchema.pre('save',(async function(next) {
-  if (!this.isNew) return next();
-
-  const lastProd = await this.constructor.findOne(
-  {}, 
-  {},
-  { sort: { productID: -1}});
-  if (lastProd) {
-  this.productID= lastProd.productID + 1;
-  } else {
-  this.productID = 1;
-  }
-  next();
-  })
-  );
-
-productSchema.pre(/^find/, function(next) {
-  this.find({ active: { $ne: false } });
-  next();
-});
-
-productSchema.virtual("reviews", {
-  ref: "Review",
-  foreignField: "product",
-  localField: "_id",
-});
-
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,17 +1,14 @@
+const express = require('express');
+const router = express.Router();
 const cartController = require('../controllers/cartController');
 const authController = require('../controllers/authController');
-const userController = require('../controllers/userController');
-const express = require('express');
 
-const router = express.Router({ mergeParams: true });
+
 router.use(authController.protect);
 
-router
-  .route('/')
-  .get(cartController.getUsersCarts)
-  .delete(cartController.dropCart);
-router.patch('/item/:itemID', cartController.updateCartItem);
+router.get('/', cartController.getCart);
+router.put('/:productId', cartController.updateProductCount);
 router.post('/addToCart', cartController.addToCart);
-router.post('/removefromCart', cartController.RemoveCartItem);
-router.delete('/dropCart', userController.getMe, cartController.dropCart);
+router.delete('/removeFromCart/:productId', cartController.deleteFromCart);
+
 module.exports = router;
